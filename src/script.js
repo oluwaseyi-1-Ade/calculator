a3f350b82832cbebca35a9cad4d2ddb4f29f52bc
oluwaseyi-1-Ade <154804947+oluwaseyi-1-Ade@users.noreply.github.com>
2024-11-19 22:59:46 +0100

    updates

--- src/script.js
+++ src/script.js
@@ -1,8 +1,26 @@
 const numbers = document.querySelectorAll("#num");
-const input = document.querySelector("#cal-input")
+const input = document.querySelector("#cal-input");
+const clearBtn = document.querySelector("#clear-btn");
+const deleteBtn = document.querySelector("#delete");
 
+clearBtn.classList.add("active:bg-blue-800");
 numbers.forEach((num) => {
+    num.classList.add("active:bg-gray-400");
+
     num.onclick = () => {
-        input.value += num.value
+        input.value += num.value;
+    };
+});
+
+clearBtn.onclick = () => {
+    input.value = "";
+};
+
+deleteBtn.onclick = () => {
+    let inputArray = [...input.value];
+    for (let i = 0; i < inputArray.length; i++) {
+
+        currentInput = inputArray.pop([i]);
+        console.log(currentInput);
     }
-})
+};
